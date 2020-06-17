15810d1f7df0c85966573580ce3ec250652256ea
Hamid Khodabandehloo <hamidkhbl@gmail.com>
2020-06-17 09:54:22 -0600

    add offer completion prediction

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from app import app
+import pandas as pd
 from flask import Flask, request, redirect, render_template, flash, url_for, session, send_from_directory, abort
 from datetime import datetime
 import sys
@@ -6,12 +7,14 @@ sys.path.append('app/code')
 from plot import plot
 sys.path.append('model')
 from filter_offer import user_offers_history, load_data, user_history_analyzer
+from sklearn.externals import joblib
 
 @app.route("/")
 @app.route("/index", methods = ["GET", "POST"])
 def index():
     df1_html = ""
     df1=""
+    new_offer=""
     if request.method == "POST":
         req = request.form
         user_id = req.get("user_id")
@@ -21,9 +24,22 @@ def index():
     return render_template("public/index.html",  df1 = df1_html, count = len(df1), new_offer = new_offer)
 
 
-@app.route("/dashboard")
-def dashboard():
-    return render_template("public/dashboard.html")
+@app.route("/predict", methods = ["GET", "POST"])
+def predict():
+    message = ""
+    if request.method == "POST":
+        req = request.form
+        age = req.get("age")
+        income = req.get("income")
+        gender = req.get("gender")
+
+        model = joblib.load("model/model.pkl")
+        query =  pd.DataFrame({'age': [int(age)], 'gender': [ 1 if gender == "Male" else 0], 'income': [int(income)]})
+        #query =  [int(age), 1 if gender == "Male" else 0, int(income)]
+        completed = model.predict(query)
+
+        message = "{} users with the age of {} who has an income of around {} are most likely to {} offers.".format("Male" if gender == 1 else "Female", age, income, "complete" if completed == 1 else "not compelete")
+    return render_template("public/predict.html", message = message)
 
 @app.route("/about")
 def about():
